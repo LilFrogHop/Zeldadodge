594f448f3064ce9e859a759d1caef9ef4008556c
DESKTOP-BFDLU2I\scott <scottyoung6997@gmail.com>
2020-11-23 20:12:48 -0500

    InitCommit

---
+++ Source/Zeldadodge/Zeldadodge.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Zeldadodge.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Zeldadodge, "Zeldadodge" );
